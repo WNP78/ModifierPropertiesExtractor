064469e2dc96bf9d68dc3ec123b5c941328e4999
WNP78 <tgbyhnujmrfvedcwsx@gmail.com>
2019-01-25 21:09:24 +0000

    Loads dll, finds all modifier types.

--- ModifierPropertiesExtractor/Program.cs
+++ ModifierPropertiesExtractor/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Mono.Cecil;
 using System.Text;
+using System;
 
 namespace ModifierPropertiesExtractor
 {
@@ -9,7 +8,41 @@ namespace ModifierPropertiesExtractor
     {
         static void Main(string[] args)
         {
+            Console.Write("File Path> ");
+            string filePath = Console.ReadLine();
+            if (string.IsNullOrEmpty(filePath))
+            {
+                filePath = "E:\\SR2Debug\\Game\\SimpleRockets2_Data\\Managed\\SimpleRockets2.dll";
+            }
+            ModuleDefinition module = ModuleDefinition.ReadModule(filePath);
+            
+            Console.WriteLine("Got module: " + module.Name);
 
+            foreach (TypeDefinition type in module.Types)
+            {
+                bool isMod = false;
+                TypeReference t = type.BaseType;
+                while (t != null)
+                {
+                    if (t.FullName.StartsWith("ModApi.Craft.Parts.PartModifierScript"))
+                    {
+                        isMod = true;
+                        break;
+                    }
+                    if (t.Scope.Name != module.Name) { break; }
+                    t = t.Resolve().BaseType;
+                }
+
+                if (isMod)
+                {
+                    Console.WriteLine(type.Name);
+                }
+            }
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
+
     }
 }
